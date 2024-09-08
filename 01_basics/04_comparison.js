499f3f0e61df371846d9a049d08fc6cc80f052d6
Yogesh459 <147391518+Yogesh459@users.noreply.github.com>
2024-09-08 13:35:40 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+//console.log("2"<1);  //convert first it in int then compare
+//console.log(null>0);
+//console.log(null==0);
+//console.log(null>=0); //o/p get true - because == work differently and other comparison opratoe work differently 
+//null converting in zero
+
+console.log(undefined>0);  // in undefined always get false
+
+//comparison and enquality works different.
+
+//strict check -> ===
+
+console.log("2"===2);  // check datatype as well
