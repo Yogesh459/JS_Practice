364bae2bd03033b8f19705ff1b64c0ce564af3b5
Yogesh459 <147391518+Yogesh459@users.noreply.github.com>
2024-09-08 11:28:25 +0000

    variable , datatype

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountID = 123321
+let accountEmail = "test@mail.com"
+var accountPass = "123"
+accountCity = 31
+let accountState
+
+console.log(accountEmail);
+
+console.table([accountID,accountEmail, accountPass, accountCity,accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict";  //trit as new JS version
+
+//alert(3+3) //it will not work here in node, it will work on browser
+
+//number 
+//bigint
+//string=""
+//boolean= true/false
+//null => standalon value, typeof null is object.
+// undefined =
+//symbol = used for uniqueness, used with controllers
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);     //object
+
